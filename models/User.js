b43d2901ce48a1cd1c06bbe04fb5a19c5bf9ca5f
pablo <paulandrewjeffrey@gmail.com>
2021-05-11 19:32:50 -0700

    fml

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 
 const bcrypt = require('bcrypt');
 
-const sequelize = require('../config/connection');
+const sequelize = require('../config/connections');
 
 class User extends Model {
   checkPassword(loginPw) {
