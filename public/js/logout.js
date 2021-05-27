54999e9a827233847b5c86fb3ab059eb4c4c6f5d
Joseph Cordell <joseph@jcordell.com>
2021-05-26 19:34:54 -0700

    Compresseed JS

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,13 +1,1 @@
-const logout = async () => {
-    const response = await fetch('/api/users/logout', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-    });
-    if (response.ok) {
-        document.location.replace('/');
-    } else {
-        return;
-    }
-};
-
-document.querySelector('#logout').addEventListener('click', logout);
+const logout=async()=>{const a=await fetch("/api/users/logout",{method:"POST",headers:{"Content-Type":"application/json"}});a.ok&&document.location.replace("/")};document.querySelector("#logout").addEventListener("click",logout);
